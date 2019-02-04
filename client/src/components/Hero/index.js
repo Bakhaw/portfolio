a14c081ad0c34f4d0b4906a429b382eae0b11ef7
bakhaw <fhattou@simplon.co>
2019-02-04 15:04:29 +0100

    Use of <ContentWrapper /> in <Hero />

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import ContentWrapper from '../ContentWrapper';
 import Me from '../../assets/images/me.svg';
 
 function Hero() {
   return (
-    <div className='Hero flicker-inz'>
-      <img className='Hero__image' src={Me} />
-      <h1 className='Hero__text Hero__title'>Faïssal Hattou</h1>
-      <h2 className='Hero__text'>Web developer</h2>
-      <h2 className='Hero__text'>Paris, France</h2>
-    </div>
+    <ContentWrapper>
+      <div className='Hero'>
+        <div className='Hero__HeroCard flicker-inz'>
+          <img className='Hero__HeroCard__image' src={Me} />
+          <h1 className='Hero__HeroCard__text Hero__HeroCard__title'>
+            Faïssal Hattou
+          </h1>
+          <h2 className='Hero__HeroCard__text'>Web developer</h2>
+          <h2 className='Hero__HeroCard__text'>Paris, France</h2>
+        </div>
+      </div>
+    </ContentWrapper>
   );
 }
 
