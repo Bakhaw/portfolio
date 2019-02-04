1e4c15f612d31760e6a1b485fc825432aaecfedf
bakhaw <fhattou@simplon.co>
2019-02-04 15:04:53 +0100

    Updated NavBar position (now stuck top)

--- client/src/components/NavBar/NavBrand.js
+++ client/src/components/NavBar/NavBrand.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import Logo from '../../assets/images/pineapple-purple.svg';
 import Logo from '../../assets/images/pineapple.svg';
 
 function NavBrand() {
   return (
     <div className='NavBar__NavBrand'>
       <img alt='NavBrand logo' className='NavBar__NavBrand__image' src={Logo} />
-      {/* <p className='NavBar__NavBrand__text'>FH</p> */}
     </div>
   );
 }
