550394bc0f212be019c914df0964f6accc16e098
bakhaw <fhattou@simplon.co>
2019-02-04 15:05:22 +0100

    Add <NavBar /> in Router instead of Home

--- client/src/routing/index.js
+++ client/src/routing/index.js
@@ -4,17 +4,21 @@ import Contact from '../screens/Contact';
 import Home from '../screens/Home';
 import Project from '../screens/Project';
 import Skills from '../screens/Skills';
+import NavBar from '../components/NavBar';
 
 function Router() {
   return (
     <HashRouter>
-      <Switch>
-        {/* :projectName is the associated to the current project images folder */}
-        <Route path='/project/:projectName' component={Project} />
-        <Route path='/skills' component={Skills} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/' component={Home} />
-      </Switch>
+      <div>
+        <NavBar />
+        <Switch>
+          {/* :projectName is the associated to the current project images folder */}
+          <Route path='/project/:projectName' component={Project} />
+          <Route path='/skills' component={Skills} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/' component={Home} />
+        </Switch>
+      </div>
     </HashRouter>
   );
 }
--- client/src/screens/Home/index.js
+++ client/src/screens/Home/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import CardsContainer from '../../components/CardsContainer';
 import Footer from '../../components/Footer';
-
 import Hero from '../../components/Hero';
-import NavBar from '../../components/NavBar';
 
-function Home({ location }) {
+function Home() {
   return (
     <div>
-      <NavBar location={location.pathname} />
-
       <Hero />
-      {/* <CardsContainer /> */}
       <Footer />
     </div>
   );
